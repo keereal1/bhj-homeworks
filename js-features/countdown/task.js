eda68d6273b624158594ee893ef45e423363b392
krlo <kirill.osadchenko@gmail.com>
2024-08-30 08:38:04 +0500

    first commit countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,10 @@
-let timer = document.getElementById('timer');
+let timer = document.getElementById("timer");
 let countdown = function() {
 
 	timer.textContent--;
 	if (timer.textContent == 0) {
 		clearInterval(intervalId);
-		alert('Вы победили в конкурсе!');
+		alert("Вы победили в конкурсе!");
 	}
 }
 intervalId = setInterval(countdown, 1000);
