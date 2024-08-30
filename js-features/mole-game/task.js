1f93147da06d101e5ae6c5a79c4a3faa2ef806d7
krlo <kirill.osadchenko@gmail.com>
2024-08-30 08:39:04 +0500

    first commit mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,27 +1,27 @@
 let getHole = index => document.getElementById(`hole${index}`);
-let deadMole = document.getElementById('dead');
-let miss = document.getElementById('lost');
+let deadMole = document.getElementById("dead");
+let miss = document.getElementById("lost");
 
 for (i = 1; i < 10; i += 1) {
 	let hole = document.getElementById(`hole${i}`)
 
-	function counter() {
-		if (hole.className.includes('hole_has-mole')) {
+	function clickAction() {
+		if (hole.className.includes("hole_has-mole")) {
 			deadMole.textContent++;
 		} else {
 			miss.textContent++;
 		}
 
 		if (deadMole.textContent == 3) {
-			alert('Победа!');
+			alert("Победа!");
 		} else if (miss.textContent == 5) {
-			alert('Вы проиграли!');
+			alert("Вы проиграли!");
 		};
 	}
 
 
 
-	hole.onclick = counter;
+	hole.onclick = clickAction;
 
 
 }
