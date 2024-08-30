a2de963b36df96447d351de0a6d87b3c03f319f7
krlo <kirill.osadchenko@gmail.com>
2024-08-30 08:23:29 +0500

    first commit cookie-clicker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,14 @@
+let counter = document.getElementById('clicker__counter');
+let cookie = document.getElementById('cookie');
+
+function clicker() {
+	counter.textContent++;
+	if (counter.textContent % 2 === 0) {
+		cookie.width = '200';
+	} else {
+		cookie.width = '300';
+	}
+}
+
+
+cookie.onclick = clicker;
---
+++ js-features/mole-game/task.js
@@ -0,0 +1,27 @@
+let getHole = index => document.getElementById(`hole${index}`);
+let deadMole = document.getElementById('dead');
+let miss = document.getElementById('lost');
+
+for (i = 1; i < 10; i += 1) {
+	let hole = document.getElementById(`hole${i}`)
+
+	function counter() {
+		if (hole.className.includes('hole_has-mole')) {
+			deadMole.textContent++;
+		} else {
+			miss.textContent++;
+		}
+
+		if (deadMole.textContent == 3) {
+			alert('Победа!');
+		} else if (miss.textContent == 5) {
+			alert('Вы проиграли!');
+		};
+	}
+
+
+
+	hole.onclick = counter;
+
+
+}
