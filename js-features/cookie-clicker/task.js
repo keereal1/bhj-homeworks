37dd206966b073ebd47ad33fd83716ea53888ac5
krlo <kirill.osadchenko@gmail.com>
2024-08-31 20:46:46 +0500

    Опитимзация кода cookie-clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,14 +1,6 @@
 let counter = document.getElementById("clicker__counter");
 let cookie = document.getElementById("cookie");
 
-function clicker() {
-	counter.textContent++;
-	if (counter.textContent % 2 === 0) {
-		cookie.width = '200';
-	} else {
-		cookie.width = '300';
-	}
+cookie.onclick = function () {
+    cookie.width = ++counter.textContent % 2 ? 300 : 200;
 }
-
-
-cookie.onclick = clicker;
